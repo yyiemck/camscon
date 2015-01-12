7143d742a402b350526427af81cdddd26c173c3e
dayer <dayer@jr.dayer-kyprosa>
2015-01-12 19:07:58 +0900

    1/12 19:07

--- dbinfo.php
+++ dbinfo.php
@@ -1,17 +1,24 @@
 <?php
 	class dbinfo {
-		private $dbid = "root";	
-		private $dbpass = "tkfakeh";
-		private $dbname ="mydb";
-		private $dbhost = "yj.dev";
+		private $dbid;
+		private $dbpass;
+		private $dbname;
+		private $dbhost;
 		public $dblink;
 		
-		public function dbConnect(){
+		public function dbConnect(){	
 			$this->dblink = new mysqli($this->dbhost,$this->dbid,$this->dbpass,$this->dbname);
 			if(mysqli_connect_errno()){
 				echo "데이터베이스 접속 실패".mysqli_connect_error();
 			}
 			return $this->dblink;
+		}
+
+		public function __construct(){
+			$this->dbhost = $_SERVER["MYSQL_HOST"];
+			$this->dbid = $_SERVER["MYSQL_USER"];
+			$this->dbpass = $_SERVER["MYSQL_PSWD"];
+			$this->dbname = $_SERVER["MYSQL_DB"];
 		}	
 	}
 ?>
