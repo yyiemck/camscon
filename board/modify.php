f15ef5760dd8ae8cfdda00ef6c5cae2a88a8a7de
dayer <dayer@jr.dayer-kyprosa>
2015-01-13 18:30:46 +0900

    1/13 18:30

--- board/modify.php
+++ board/modify.php
@@ -13,7 +13,7 @@ session_start();
 		margin: 20px;
 	}
 	.container {
-		max-width: 600px;
+		max-width: 1200px;
 		margin: auto;
 		margin-top: 30px;
 	}
@@ -37,6 +37,10 @@ session_start();
 		max-width: 400px;
 		margin: 5px;
 	}
+	.text3{
+		max-width: 700px;
+		margin: 5px;
+	}
 	.tag {
 		width: 90px;
 		margin: 5px;
@@ -45,6 +49,24 @@ session_start();
 	}
 	.box {
 		max-width: 400px;
+	}
+	.img_size {
+		width: 125px;
+		height: 125px;
+	}
+	.btn-group {
+		display: block;
+	}
+	.mybtn {
+		width: 100px;
+	}
+	.mybtn_2 {
+		height:34px;
+	}
+	.divcol {
+		border-right: 1px solid orange;
+		margin-right: 20px;
+		height: 550px;
 	}
 	</style>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -56,9 +78,32 @@ session_start();
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="../../package/bootstrap/js/bootstrap.min.js"></script>
 	<div class="container">
+		<div class="col-md-2 divcol">
+			<div class="img_btn">
+				<img class="img_size" src="../images.jpg"></img><br>
+				<div class="btn-group">
+					<button type="button" class="mybtn btn btn-danger">
+						<?php
+						echo "$_SESSION[nickname]";	
+						?>	
+					</button>
+					<button type="button" class="mybtn_2 btn btn-danger dropdown-toggle" data-toggle="dropdown">
+						<span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="../dblogout.php"><i class="glyphicon glyphicon-off"></i> 로그아웃</a></li>
+						<li><a href="../form_modifypwd.php"><i class="glyphicon glyphicon-edit"></i> 비밀번호 변경</a></li>
+						<li class="divider"></li>
+						<li><a href="../form_withdraw.php"><i class="glyphicon glyphicon-trash"></i> 회원탈퇴</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9">
 		<ul class="nav nav-pills">
- 			<li role="presentation"><a href="#">게시판</a></li>
-  			<li role="presentation"><a href="write.php">글쓰기</a></li>
+ 			<li role="presentation"><a href="#">게시판1</a></li>
+  			<li role="presentation"><a href="#">게시판2</a></li>
+  			<li role="presentation"><a href="#">게시판3</a></li>
 		</ul>
 		<form action="insert.php" method="POST">
 			<table class="table1 table table-bordered">
@@ -67,7 +112,7 @@ session_start();
 				</tr>
 				<tr>
 					<td class="tag">이름</td>
-					<td><?php echo "$_SESSION[nickname]"?></td>
+					<td>&nbsp;<?php echo "$_SESSION[nickname]"?></td>
 				</tr>
 				<tr>
 					<td class="tag">비밀번호</td>
@@ -84,7 +129,7 @@ session_start();
 				<tr>
 					<td class="tag">내용</td>
 					<td class="box">
-						<textarea name="content" class="text2 form-control" cols="20" rows="10"></textarea>
+						<textarea name="content" class="text3 form-control" cols="20" rows="10"></textarea>
 					</td>
 				</tr>
 				<tr>
@@ -96,6 +141,7 @@ session_start();
 				</tr>
 			</table>
 		</form>
+		</div>
 	</div>
 </body>
 </html>
--- board/read.php
+++ board/read.php
@@ -13,7 +13,7 @@ session_start();
 		margin: 20px;
 	}
 	.container {
-		max-width: 600px;
+		max-width: 1200px;
 		margin: auto;
 		margin-top: 30px;
 	}
@@ -28,14 +28,7 @@ session_start();
 	}
 	.table1 {
 		max-width: 580px;
-	}
-	.text1 {	
-		max-width: 100px;
-		margin: 5px;
-	}
-	.text2{
-		max-width: 400px;
-		margin: 5px;
+		border-radius: 8px;
 	}
 	.tag {
 		width: 90px;
@@ -45,7 +38,25 @@ session_start();
 	}
 	.content {
 		max-width: 400px;
-		height: 300px;
+		height: 360px;
+	}
+	.img_size {
+		width: 125px;
+		height: 125px;
+	}
+	.btn-group {
+		display: block;
+	}
+	.mybtn {
+		width: 100px;
+	}
+	.mybtn_2 {
+		height:34px;
+	}
+	.divcol {
+		border-right: 1px solid orange;
+		margin-right: 20px;
+		height: 550px;
 	}
 	</style>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -57,9 +68,32 @@ session_start();
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="../../package/bootstrap/js/bootstrap.min.js"></script>
 	<div class="container">
+		<div class="col-md-2 divcol">
+			<div class="img_btn">
+				<img class="img_size" src="../images.jpg"></img><br>
+				<div class="btn-group">
+					<button type="button" class="mybtn btn btn-danger">
+						<?php
+						echo "$_SESSION[nickname]";	
+						?>	
+					</button>
+					<button type="button" class="mybtn_2 btn btn-danger dropdown-toggle" data-toggle="dropdown">
+						<span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="../dblogout.php"><i class="glyphicon glyphicon-off"></i> 로그아웃</a></li>
+						<li><a href="../form_modifypwd.php"><i class="glyphicon glyphicon-edit"></i> 비밀번호 변경</a></li>
+						<li class="divider"></li>
+						<li><a href="../form_withdraw.php"><i class="glyphicon glyphicon-trash"></i> 회원탈퇴</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9">
 		<ul class="nav nav-pills">
- 			<li role="presentation"><a href="#">게시판</a></li>
-  			<li role="presentation"><a href="write.php">글쓰기</a></li>
+ 			<li role="presentation"><a href="#">게시판1</a></li>
+  			<li role="presentation"><a href="#">게시판2</a></li>
+  			<li role="presentation"><a href="#">게시판3</a></li>
 		</ul>
 		<form action="insert.php" method="POST">
 			<table class="table1 table table-bordered">
@@ -84,6 +118,7 @@ session_start();
 				</tr>
 			</table>
 		</form>
+	</div>
 	</div>
 </body>
 </html>
